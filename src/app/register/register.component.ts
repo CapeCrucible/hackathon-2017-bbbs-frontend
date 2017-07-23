3cae994e821a8b0764ad97c9377b132c529b01ac
Cameron Hutchison <chutch112292@gmail.com>
2017-07-22 22:37:33 -0500

    Fix creating user call

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { HttpWrapper } from '../http-wrapper.service';
 import { environment } from '../../environments/environment';
 import { RegisterAccountRequest } from './register-account.request';
@@ -12,27 +12,27 @@ import { getCode } from '../forms/state-picker/state.model';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  regForm: FormGroup;
   private baseUrl: string;
-  regFormControl: FormControl;
+  regForm: FormGroup;
+  stateControl: FormControl;
 
   constructor(private http: HttpWrapper, private formBuilder: FormBuilder) {
     this.baseUrl = environment.apiUrl;
-    this.regFormControl = new FormControl();
+    this.stateControl = new FormControl();
   }
 
   ngOnInit() {
     this.regForm = this.formBuilder.group({
-      email: [''],
-      username: [''],
-      password: [''],
-      firstName: [''],
-      lastName: [''],
-      addressLine1: [''],
-      addressLine2: [''],
-      city: [''],
-      statePicker: [''],
-      zipCode: [''],
+      email: '',
+      username: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      addressLine1: '',
+      addressLine2: '',
+      city: '',
+      statePicker: '',
+      zipCode: '',
     });
   }
 
@@ -50,21 +50,19 @@ export class RegisterComponent implements OnInit {
         addressLine2: this.regForm.controls['addressLine2'].value,
         city: this.regForm.controls['city'].value,
         state: {
-          code: getCode(this.regFormControl.value),
-          name: this.regFormControl.value
+          code: getCode(this.stateControl.value),
+          name: this.stateControl.value
         },
         zipCode: this.regForm.controls['zipCode'].value
       },
       contactInfo: {
-        // userId: 1234, // This shouldn't be required but for some reason it is
         email: this.regForm.controls['email'].value,
         phoneNumber: '555-123-4567'
       },
-      interests: [
-      ]
+      interests: []
     };
 
-    this.http.post(this.baseUrl + 'CreateConsolidatedUser', request);
+    this.http.post(this.baseUrl + 'CreateConsolidatedUser', request).subscribe();
     console.log(this.baseUrl + 'CreateConsolidatedUser', request);
   }
 }
