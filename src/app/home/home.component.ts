435429b48f52dca4aad48308e371dc08352d8310
Joyce <AJOYCE@corp.vha.ad>
2017-07-23 12:25:43 -0500

    --ammend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpWrapper } from '../http-wrapper.service';
-import { environment } from '../../environments/environment';
 import { LoginRequest } from './login.request';
 import { UserAccount } from '../user/user-account.model';
 import { LoginService } from '../login.service';
@@ -12,16 +10,13 @@ import { LoginService } from '../login.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private baseUrl: string;
   loginForm: FormGroup;
   user: UserAccount;
 
   constructor(
-    private http: HttpWrapper,
     private formBuilder: FormBuilder,
     private loginService: LoginService
   ) {
-    this.baseUrl = environment.apiUrl;
     this.loginService = loginService;
   }
 
@@ -39,9 +34,6 @@ export class HomeComponent implements OnInit {
         password: this.loginForm.controls['password'].value
       }
     };
-
-    this.http.post<UserAccount>(this.baseUrl + 'Login', request)
-      .subscribe(reply => this.loginService.login(reply));
-    console.log(this.baseUrl + 'Login', request);
+    this.loginService.login(request);
   }
 }
--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { UserAccount } from './user/user-account.model';
+import { environment } from './../environments/environment';
+import { LoginRequest } from './home/login.request';
+import { HttpWrapper } from './http-wrapper.service';
 
 @Injectable()
 export class LoginService {
   user: UserAccount;
+  private baseUrl: string;
 
-  constructor() { }
+  constructor(
+    private http: HttpWrapper,
+  ) {
+    this.baseUrl = environment.apiUrl;
+   }
 
-  login(user: UserAccount) {
-    this.user = user;
-    console.log('Logged In');
+  login(request: LoginRequest) {
+    this.http.post<UserAccount>(this.baseUrl + 'Login/Login', request)
+      .subscribe(reply => this.user = reply);
+
+    console.log(this.baseUrl + 'Login/Login', request);
+    console.log(!!this.user ? 'Logged In' : 'Login Failed');
   }
 
   logout() {
