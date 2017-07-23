b8fdcc87084ef31bac625359abf7e785fcb049ac
Joyce <AJOYCE@corp.vha.ad>
2017-07-22 23:25:40 -0500

    Added login fields to the homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpWrapper } from '../http-wrapper.service';
+import { environment } from '../../environments/environment';
+import { LoginRequest } from './login.request';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+private baseUrl: string;
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpWrapper, private formBuilder: FormBuilder) {
+    this.baseUrl = environment.apiUrl;
   }
 
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
+  }
+
+  register() {
+    const request: LoginRequest = {
+      loginCredentials: {
+        username: this.loginForm.controls['username'].value,
+        password: this.loginForm.controls['password'].value
+      }
+    };
+
+    this.http.post(this.baseUrl + 'Login', request).subscribe();
+    console.log(this.baseUrl + 'Login', request);
+  }
 }
---
+++ src/app/home/login.request.ts
@@ -0,0 +1,5 @@
+import { LoginCredentials } from './login-credentials.model';
+
+export interface LoginRequest {
+  loginCredentials: LoginCredentials;
+}
